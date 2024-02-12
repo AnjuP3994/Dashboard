74655c6e62fabf758e9ce7e17b3e7ffe7881b05c
Anju <anjup3994@gmail.com>
2024-02-12 14:05:39 +0530

    dashboard

--- src/Services/baseURL.js
+++ src/Services/baseURL.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:3003'
+export const BASE_URL = 'https://dashboard-sgsf.onrender.com'
 
-// http://localhost:3003/
-// https://dashboard-sgsf.onrender.com/
+// http://localhost:3003
+// https://dashboard-sgsf.onrender.com
